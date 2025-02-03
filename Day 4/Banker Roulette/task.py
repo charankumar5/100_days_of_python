cf5c855eae02451fb56b8c462ce0b22c90037715
charan <kumar.charanporeddy@gmail.com>
2025-02-03 22:27:31 +0100

    Day 3

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,11 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# option 1
+bill_payer = random.choice(friends)
+print(bill_payer)
+
+# option 2:
+index = random.randint(0,4)
+print(friends[index])
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -24,3 +24,17 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+import random
+options = [rock, paper, scissors]
+your_option = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors:"))
+print(f"You chose: \n {options[your_option]}")
+
+computer_option = random.randint(0,2)
+print(f"Computer chose:\n {options[computer_option]}" )
+
+if your_option < computer_option:
+    print("Computer win.")
+elif your_option == computer_option:
+    print("Draw")
+else:
+    print("You win.")
