77d58f49782584a94daf7b3d829cf4feaa0b62ac
charan <kumar.charanporeddy@gmail.com>
2025-02-11 22:44:12 +0100

    update calculator app

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,5 @@
 from lib2to3.pgen2.grammar import opmap_raw
+import art
 
 
 def add(n1, n2):
@@ -21,16 +22,16 @@ operations = { "+" : add ,  "-" : subtract,  "*" : multiply,  "/" : divide}
 
 def calculation():
     calculator_operation = True
-    input1 = float(input("Please enter the first number: "))
+    input1 = float(input("What's the first number? "))
     while calculator_operation:
         for symbol in operations:
             print(symbol)
-        operation = input("Enter the mathematical operator: ")
-        print("You have selected this operation to perform: " + operation)
-        input2 = float(input("Please enter the second number: "))
-        answer = operations[operation](input1, input2)
-        print(f"final operation: {answer}")
-        user_preference = input("You want to continue with the previous results, type 'yes' or 'no': ")
+        operation = input("Pick an operation: ")
+        # print("You have selected this operation to perform: " + operation)
+        input2 = float(input("What's the next number? "))
+        answer = float(operations[operation](input1, input2))
+        print(f"{input1} {operation} {input2} = {answer}")
+        user_preference = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start new calculation: " )
         if user_preference in " yes ":
             input1 = answer
         else:
