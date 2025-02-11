02cb9562205528886febddfdf043c7e3ee482f8d
charan <kumar.charanporeddy@gmail.com>
2025-02-11 06:24:48 +0100

    Bid game

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,36 +1,32 @@
-import os
-from subprocess import call
-call(["python", "art.py"])
-bid_dictionary = {}
+from art import logo
 # TODO-1: Ask the user for input
-def bid():
-    name = input(str("What is your name?: "))
-    bid_price = input(str("What is your bid?: $"))
-    # TODO-2: Save data into dictionary {name: price}
-    bid_dictionary[name] = int(bid_price)
-    print(bid_dictionary)
-
+print("Welcome to the secret auction program.")
+Bids = {}
+name  = input("Enter your name? ")
+bid_amount = int(input("What's your bid?$ "))
+Bids[name] = bid_amount
 # TODO-3: Whether if new bids need to be added
-continue_bidding = True
-name = input(str("What is your name?: "))
-bid_price = input(str("What is your bid?: $"))
+while True:
+    confirm = input("Are there any other bidders? Type 'yes' or 'no'." ).lower()
+    if confirm == "no":
+        break
+    else:
+        name  = input("Enter your name? ")
+        bid_amount = int(input("What's your bid?$ "))
+        Bids[name] = bid_amount
 # TODO-2: Save data into dictionary {name: price}
-bid_dictionary[name] = int(bid_price)
-print(bid_dictionary)
-while continue_bidding:
-    next_bid = input(str("Are there any other bidders? Type 'Yes' or 'no'."))
-    if next_bid == "Yes":
-        os.system('cls')
-        bid()
-    elif next_bid == "no":
-        # TODO-4: Compare bids in dictionary
-        winner = ""
-        for bidder in bid_dictionary:
-            bid_amount = bid_dictionary[bidder]
-            for alpha in bid_dictionary:
-                if bid_amount > bid_dictionary[alpha]:
-                    winner = bidder
-        print(winner)
+max_bid = 0
+winner = ""
+for key, value in Bids.items():
+    if value > max_bid:
+        max_bid = value
+        winner = key
+print(f"The winner is {winner} with a bid of ${max_bid}")        
+
+
+
+
+
 
 
 
