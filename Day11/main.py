bb4701270db8102c1fc2aa4fe5e8942c2ac52d98
charan <kumar.charanporeddy@gmail.com>
2025-02-12 21:09:44 +0100

    BlackJack game

---
+++ Day11/main.py
@@ -0,0 +1,25 @@
+import art
+import random
+print(art.logo)
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+user_choice = input("Do you want to play a game of Blackjack? Type 'y' orr 'n' : ")
+
+if user_choice is "y":
+    print()
+else:
+    users_hand = []
+    users_hand.extend(random.sample(cards,2))
+    computers_hand = []
+    computers_hand.extend(random.sample(cards,3))
+    print(f"    Your final hand: {users_hand}, final score: {sum(users_hand)}")
+    print(f"    Computer's  final hand: {computers_hand}, final score: {sum(computers_hand)}")
+    if (sum(users_hand) > sum(computers_hand)) and (sum(users_hand) < 21) and (sum(computers_hand) < 21):
+        print("You Win!")
+    elif sum(users_hand) == sum(computers_hand):
+        print("Draw")
+    elif (sum(computers_hand) > 21) and (sum(users_hand) < 21):
+        print("You Win!")
+    else:
+        print("You lose")
